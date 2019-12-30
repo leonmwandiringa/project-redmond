127a9cf641c5d61d893f6dea799cc5931f43d73a
leontinashe <tinashe.leon@yahoo.com>
2019-12-30 11:45:21 +0200

    now sending data to server

--- console/services/api/metrics.go
+++ console/services/api/metrics.go
@@ -17,13 +17,16 @@ func GetData() (interface{}, error){
 		return nil, errors.New("an error ocured. Docker related")
 	}
 	dataToSend := make(map[string]interface{})
-	dataToSend["container_count"] = getContainersRunning()
-	dataToSend["image_count"] = numberOfImagesCreated()
+	
+	imageMetrics, _ := sortImagesMetrics(getImages())
+	containerMetrics, _ := sortContainersMetrics(getContainers())
+	dataToSend["container_metrics"] = containerMetrics
+	dataToSend["image_metrics"] = imageMetrics
 
 	return dataToSend,nil
 }
 
-func getContainersRunning() []string{
+func getContainers() []string{
 	var containers []string
 	containersRunning, _ := exec.Command("docker", "container", "ps", "-aq").CombinedOutput()
 	for _, container := range strings.Split(string(containersRunning), "\n"){
@@ -32,11 +35,36 @@ func getContainersRunning() []string{
 	return containers
 }
 
-func numberOfImagesCreated() []string{
+func getImages() []string{
 	var allImages []string
 	images, _ := exec.Command("docker", "image", "ls", "-aq").CombinedOutput()
 	for _, image := range strings.Split(string(images), "\n"){
 		allImages = append(allImages, image)
 	}
 	return allImages
+}
+
+func sortImagesMetrics(images []string) (interface{}, error){
+	imagesMetrics := make(map[string]interface{})
+	if images == nil || len(images) < 1{
+		return imagesMetrics, errors.New("there are no images currently")
+	}
+	for _, image := range images{
+		imageMetric, _ := exec.Command("docker", "image", "inspect", image).CombinedOutput()
+		imagesMetrics[image] = string(imageMetric)
+	}
+
+	return imagesMetrics, nil
+}
+
+func sortContainersMetrics(containers []string) (interface{}, error){
+	containersMetrics := make(map[string]interface{})
+	if containers == nil || len(containers) < 1{
+		return containersMetrics, errors.New("there are no containers currently")
+	}
+	for _, container := range containers{
+		containerMetric, _ := exec.Command("docker", "container", "inspect", container).CombinedOutput()
+		containersMetrics[container] = string(containerMetric)
+	}
+	return containersMetrics, nil
 }
